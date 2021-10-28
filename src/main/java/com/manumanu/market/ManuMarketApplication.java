81091af1439efcec5a61298bf667b378387308d3
ALEX <mifito1@gmail.com>
2021-10-27 19:59:37 -0500

    Spring

---
+++ src/main/java/com/manumanu/market/ManuMarketApplication.java
@@ -0,0 +1,13 @@
+package com.manumanu.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManuMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManuMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manumanu/market/ManuMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manumanu.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManuMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
